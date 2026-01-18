33417e3467b8a97b9620c3b6614b8797633b69ec
adeliaterco <adeliarosaterco@gmail.com>
2026-01-18 08:52:19 -0300

    hhdh

--- src/pages/Module1Dashboard.tsx
+++ src/pages/Module1Dashboard.tsx
@@ -77,7 +77,7 @@ const Module1Dashboard = () => {
               Welcome, {user.nombre}!
             </h1>
             <p className="text-muted-foreground text-lg">
-              Your progress in PRP RECONQUISTA
+              Your progress in PRP RECONNECTION
             </p>
           </motion.div>
 
